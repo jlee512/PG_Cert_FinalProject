ad8dd523cc37b376fac43386f72e2cbb09803df5
jlee512 <julian.j.lees@gmail.com>
2017-05-31 21:28:34 +1200

    initial commit test

--- generateTestDatabase.sql
+++ generateTestDatabase.sql
@@ -1,1 +1,9 @@
-DROP 
+/*Drop tables in correct suitable order as to avoid invalidating foreign key constraints*/
+
+DROP TABLE IF EXISTS posted_comments;
+DROP TABLE IF EXISTS posted_multimedia;
+DROP TABLE IF EXISTS uploaded_articles;
+DROP TABLE IF EXISTS registered_users;
+
+
+
