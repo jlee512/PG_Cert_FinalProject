fe7d58bbcbc8bdb3a043ace9fac58b4bdd677c42
cadechalain <cbla080@aucklanduni.ac.nz>
2017-06-02 12:14:13 +1200

    Working on AddCommentAttempt servlet.

--- src/login/system/servlets/AddCommentAttempt.java
+++ src/login/system/servlets/AddCommentAttempt.java
@@ -1,10 +1,19 @@
 package login.system.servlets;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by catherinedechalain on 1/06/17.
  */
 public class AddCommentAttempt extends HttpServlet {
 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
 }
