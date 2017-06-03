0acfc7d792e329666e3d8ca383893f1175bce3dc
cadechalain <cbla080@aucklanduni.ac.nz>
2017-06-03 12:17:47 +1200

    Updated AddCommentServer

--- src/login/system/servlets/AddCommentAttempt.java
+++ src/login/system/servlets/AddCommentAttempt.java
@@ -1,8 +1,6 @@
 package login.system.servlets;
 
-import login.system.dao.CommentDAO;
-import login.system.dao.User;
-import login.system.dao.UserDAO;
+import login.system.dao.*;
 import login.system.db.MySQL;
 
 import javax.servlet.http.HttpServlet;
@@ -24,14 +22,27 @@ public class AddCommentAttempt extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response){
         MySQL DB = new MySQL();
         Date currentDate = new Date(System.currentTimeMillis());
-
         String content = request.getParameter("comment_body");
         String username = request.getParameter("username");
-        //article to be passed as object from previous page.
-
         User currentUser = UserDAO.getUser(DB, username);
         int userID = currentUser.getUser_id();
+
+        //Depending on whether this is a top level comment or a reply to a previous comment, either a Comment object or an Article object will be passed.
+
         String status = CommentDAO.addComment(DB, userID, 0, 0, currentDate, content);
         System.out.println(status);
+    }
+
+    protected String addTopLevelComment(Article article, MySQL DB, int userID, Date date, String content){
+        int articleID = article.getArticle_id();
+        String status = CommentDAO.addComment(DB, userID, articleID, 0, date, content);
+        return status;
+    }
+
+    protected String addReplyComment(Comment comment, MySQL DB, int userID, Date date, String content){
+        int parentCommentID = comment.getCommentID();
+        int articleID = comment.getArticleID();
+        String status = CommentDAO.addComment(DB, userID, articleID, parentCommentID, date, content);
+        return status;
     }
 }
