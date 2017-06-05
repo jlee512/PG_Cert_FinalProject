37f1c4a13d1df0bdf089489783392e64966c891d
cadechalain <cbla080@aucklanduni.ac.nz>
2017-06-06 09:31:27 +1200

    ShowNestedComments servlet

---
+++ src/login/system/servlets/ShowNestedComments.java
@@ -0,0 +1,49 @@
+package login.system.servlets;
+
+import login.system.dao.Comment;
+import login.system.dao.CommentDAO;
+import login.system.db.MySQL;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+/**
+ * Created by cbla080 on 5/06/2017.
+ */
+public class ShowNestedComments extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        MySQL DB = new MySQL();
+        int parentCommentID = Integer.parseInt(request.getParameter("parentCommentID"));
+        List<Comment> commentList = CommentDAO.getNestedComments(DB, parentCommentID);
+
+        response.setContentType("application/json");
+        PrintWriter out = response.getWriter();
+        JSONArray commentDetails = new JSONArray();
+
+        for (Comment comment : commentList){
+            JSONObject jsonComment = new JSONObject();
+            jsonComment.put("comment_id", comment.getCommentID());
+            jsonComment.put("author_id", comment.getAuthorID());
+            jsonComment.put("article_id", comment.getArticleID());
+            jsonComment.put("parent_comment_id", comment.getParentCommentID());
+            jsonComment.put("timestamp", comment.getTimestamp());
+            jsonComment.put("content", comment.getContent());
+            jsonComment.put("is_parent", comment.getIsParent());
+            commentDetails.add(jsonComment);
+        }
+
+        commentDetails.toJSONString();
+        out.println(commentDetails);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
