99cf150664b98d8a6e856470d7efa40e9460e419
jlee512 <jlee512@aucklanduni.ac.nz>
2017-06-13 13:14:36 +1200

    test bugfix for comments

--- web/Javascript/show_comment_replies.js
+++ web/Javascript/show_comment_replies.js
@@ -55,8 +55,13 @@ $(document).on("click", ".show_replies", function () {
                 type: "GET",
                 success: function loadNestedComments(msg) {
                     buttonsDiv.find(".loader-wrapper").show();
+
+                    console.log(msg.length);
+
                     if (msg.length != 0){
                         for (i = 0; i < msg.length; i++) {
+
+                            console.log("Test");
 
                             var commentContainerTemplate = $(commentPara);
                             var replyCommentsButtonPanel = $(commentContainerTemplate).find('.buttons');
@@ -152,6 +157,7 @@ function loadNestedCommentsFail(jqXHR, textStatus, errorThrown) {
     console.log(jqXHR.status);
     console.log(textStatus);
     console.log(errorThrown);
+
 }
 
 function getArticleID () {
