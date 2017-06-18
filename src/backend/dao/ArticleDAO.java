9cc74f08465b6ef5da492025a5873ed6be47d57e
jlee512 <jlee512@aucklanduni.ac.nz>
2017-06-18 15:05:05 +1200

    refactored to 'backend' folder and added javadoc style comments to object getters and setters

--- src/backend/dao/ArticleDAO.java
+++ src/backend/dao/ArticleDAO.java
@@ -11,7 +11,9 @@ import java.util.List;
  */
 public class ArticleDAO {
 
+
     /*Add a new article to the Database*/
+
     public static int addArticleToDB(MySQL DB, int author_id, String article_title, Timestamp article_timestamp, String article_body) {
 
         /*Return method status
@@ -34,7 +36,7 @@ public class ArticleDAO {
 
                 /*Execute prepared statement*/
                 stmt.executeUpdate();
-                System.out.println("Article added to the database");
+                /*If no exception is thrown, the addition has been carried out successfully*/
                 return 1;
             }
         } catch (SQLIntegrityConstraintViolationException e) {
@@ -49,8 +51,13 @@ public class ArticleDAO {
 
     }
 
+
+
+    /*---------------------------------------------------------------*/
+    /*Get an individual article by the MySQL article_id
+    * Used in the DeleteArticle and ViewArticle Servlets*/
+
     public static Article getArticle(MySQL DB, int article_id) {
-        /*----------------------------------------------------*/
 
         /*Dummy article to be returned if article not found*/
         Article article = new Article(-1, null, null, null);
@@ -78,10 +85,9 @@ public class ArticleDAO {
                         article.setArticleParameters(article_idLookup, author_username, author_firstname, author_lastname, article_titleLookup, timestampLookup, article_bodyLookup, comment_countLookup);
                         article.setAuthor_id(author_idLookup);
 
-                        System.out.println("Article retrieved from the database");
+                        /*If no exceptions are thrown, the method has been compeleted successfully*/
                     } else {
-                        /*If the article can't be found in the database, return null article*/
-                        System.out.println("Article could not be found in the database");
+                        /*If the article can't be found in the database and the null article will be returned*/
                     }
                 }
             }
@@ -91,13 +97,19 @@ public class ArticleDAO {
             e.printStackTrace();
         }
 
-        /*----------------------------------------------------------*/
+        /*Either a specific article or a null article are returned. This outcomes are recognised in the specific servlets*/
         return article;
     }
 
+
+    /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles in date order.
+    * The method takes the initial article number and a number of articles.
+    * It is called from the MainContentAccess Servlet (which is accessed via AJAX)*/
+
     public static List<Article> getfirstNArticlePreviewsByDate(MySQL DB, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -107,6 +119,8 @@ public class ArticleDAO {
                 stmt.setInt(1, numArticles);
                 stmt.setInt(2, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -115,12 +129,20 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
+
+    /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles in date order (ascending).
+    * The method takes the initial article number and a number of articles.
+    * It is called from the MainContentAccess Servlet (which is accessed via AJAX)*/
+
     public static List<Article> getfirstNArticlePreviewsByDateASC(MySQL DB, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -130,6 +152,8 @@ public class ArticleDAO {
                 stmt.setInt(1, numArticles);
                 stmt.setInt(2, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -138,12 +162,19 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
+
+    /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles in alphabetical order by title (ascending).
+    * The method takes the initial article number and a number of articles.
+    * It is called from the MainContentAccess Servlet (which is accessed via AJAX)*/
+
     public static List<Article> getfirstNArticlePreviewsByTitle(MySQL DB, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -153,6 +184,8 @@ public class ArticleDAO {
                 stmt.setInt(1, numArticles);
                 stmt.setInt(2, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -161,12 +194,19 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
+
+    /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles in alphabetical order by title (descending).
+    * The method takes the initial article number and a number of articles.
+    * It is called from the MainContentAccess Servlet (which is accessed via AJAX)*/
+
     public static List<Article> getfirstNArticlePreviewsByTitleDESC(MySQL DB, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -176,6 +216,8 @@ public class ArticleDAO {
                 stmt.setInt(1, numArticles);
                 stmt.setInt(2, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -184,12 +226,19 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
+
+     /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles in alphabetical order by author username (ascending).
+    * The method takes the initial article number and a number of articles.
+    * It is called from the MainContentAccess Servlet (which is accessed via AJAX)*/
+
     public static List<Article> getfirstNArticlePreviewsSortedByAuthor(MySQL DB, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+       /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -199,6 +248,8 @@ public class ArticleDAO {
                 stmt.setInt(1, numArticles);
                 stmt.setInt(2, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -207,12 +258,19 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
+
+    /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles in alphabetical order by author username (descending).
+    * The method takes the initial article number and a number of articles.
+    * It is called from the MainContentAccess Servlet (which is accessed via AJAX)*/
+
     public static List<Article> getfirstNArticlePreviewsSortedByAuthorDESC(MySQL DB, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -222,6 +280,8 @@ public class ArticleDAO {
                 stmt.setInt(1, numArticles);
                 stmt.setInt(2, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -230,13 +290,20 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
 
+    /*---------------------------------------------------------------*/
+    /*This method is used to access the first 'N' articles for a given author (by date descending)).
+    * This method is used on the personal and public profile pages to present a users individual article list
+    * The method takes the author_id, an initial article number and a number of articles.
+    * The servlets which use this method are accessed via AJAX*/
+
     public static List<Article> getfirstNArticlePreviewsByAuthor(MySQL DB, int fromArticle, int numArticles, int author_id) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy list of articles to be returned if article not found*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -247,6 +314,8 @@ public class ArticleDAO {
                 stmt.setInt(2, numArticles);
                 stmt.setInt(3, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -255,44 +324,51 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
-    /*Method to delete and article*/
+
+    /*---------------------------------------------------------------*/
+    /*Method to delete an article based on an article_id and an author_id (for additional security)*/
+
     public static int deleteAnArticle(MySQL DB, int article_id, int author_id) {
 
-        /*Establish connection to the database and delete the article (note: the corresponding servlet will need to confirm user log-in status prior to deletion*/
+        /*Return method status
+        * (1) Success
+        * (2) Integrity constraint violation or no articles to delete
+        * (3) SQL error
+        * (4) Database connection error*/
 
         try (Connection c = DB.connection()) {
 
-            /*Delete all comments associated with the article_id (cascading does not work with the directionality of the foreign key arrangement)*/
+            /*Step (1) Delete all comments associated with the article_id*/
             try (PreparedStatement stmt = c.prepareStatement("DELETE FROM posted_comments WHERE article_id = ?")) {
 
                 stmt.setInt(1, article_id);
-                /*Execute the prepared statement*/
+                /*Execute the comment deletion*/
                 int deleted = stmt.executeUpdate();
                 if (deleted == 0) {
-                    System.out.println("No article comments to delete");
+//                    No article comments to delete
                 } else {
-                    System.out.println("Article comments deleted");
+//                    Article comments deleted successfully
                 }
             }
 
-            /*Provided the article's comments are deleted successfully, the database should now be permitted to delete the article itself*/
+            /*Step (2) Provided the article's comments are deleted successfully, delete the article itself*/
             try (PreparedStatement stmt = c.prepareStatement("DELETE FROM uploaded_articles WHERE article_id = ? AND author_id = ?")) {
 
                 stmt.setInt(1, article_id);
                 stmt.setInt(2, author_id);
 
-                /*Execute the prepared statement*/
+                /*Execute the article deletion*/
                 int deletedArticle = stmt.executeUpdate();
                 if (deletedArticle == 0) {
-
-                    System.out.println("No comments to delete");
+//                    No articles to delete - return a corresponding status
                     return 2;
 
                 } else {
-                    System.out.println("Article successfully deleted");
+//                    Articles deleted successfully - return success status = 1
                     return 1;
                 }
             }
@@ -312,9 +388,10 @@ public class ArticleDAO {
         }
     }
 
-//    public static int editAnArticle (MySQL DB, int article_id, )
 
-    /*Extracted method during refactoring to reduce code repitition*/
+    /*---------------------------------------------------------------*/
+    /*Extracted method for processing a table of articles returned from an SQL query into a Java List of articles*/
+
     public static void getListofArticles(List<Article> articles, PreparedStatement stmt) throws SQLException {
         try (ResultSet r = stmt.executeQuery()) {
 
@@ -336,15 +413,17 @@ public class ArticleDAO {
             }
 
             if (articles.size() > 0) {
-                System.out.println("Article retrieved from the database");
+//                Articles retrieved from the database
             } else {
-                /*If the article can't be found in the database, return null article*/
-                System.out.println("Article could not be found in the database");
+//                If the article can't be found in the database, return null article (in this method, the original input List<Article>
             }
         }
     }
 
+
+    /*---------------------------------------------------------------*/
     /*Update and article method for use with the edit article servlet doPost method*/
+
     public static int updateArticle(MySQL DB, int article_id, int author_id, String article_title_update, String article_body_update, Timestamp timestamp_update) {
 
         /*Return an integer representative of the status
@@ -352,11 +431,11 @@ public class ArticleDAO {
         * (2) Integrity constraint
         * (3) Other invalid update request
         * (4) Database connectivity issues
-        * */
+        */
 
         try (Connection c = DB.connection()) {
 
-            /*Carry out article update based on provided article_id, article_title and article_body)*/
+            /*Carry out article update based on provided article_id, article_title and article_body*/
             try (PreparedStatement stmt = c.prepareStatement("UPDATE uploaded_articles SET timestamp = ?, article_title = ?, article_body = ?  WHERE article_id = ? AND author_id = ?;")) {
 
                 /*Set the query parameters*/
@@ -366,9 +445,8 @@ public class ArticleDAO {
                 stmt.setInt(4, article_id);
                 stmt.setInt(5, author_id);
 
-                /*Execute prepared statement*/
                 stmt.executeUpdate();
-                System.out.println("Article updated in the database");
+//              If no exceptions are thrown, the article has been updated in the database and return a corresponding status code
                 return 1;
             }
 
@@ -384,9 +462,15 @@ public class ArticleDAO {
         }
     }
 
+   /*---------------------------------------------------------------*/
+    /*Method which searches the database for article titles/author usernames based on a provided search term
+    * This method takes a search term as well as an initial article number (for infinite scrolling) and a number of
+    * articles.
+    */
+
     public static List<Article> getArticlePreviewsBySearchTerm(MySQL DB, String searchTerm, int fromArticle, int numArticles) {
 
-        /*Dummy article to be returned if article not found*/
+        /*Dummy article list to be returned if articles not found or if any exceptions are thrown*/
         List<Article> articles = new ArrayList<Article>();
 
         try (Connection c = DB.connection()) {
@@ -398,6 +482,8 @@ public class ArticleDAO {
                 stmt.setInt(3, numArticles);
                 stmt.setInt(4, fromArticle);
 
+                /*This method cycles through a table of articles produced by and SQL query.
+                The parameters returned from the database are put into a list of Article instances*/
                 getListofArticles(articles, stmt);
 
             }
@@ -406,9 +492,12 @@ public class ArticleDAO {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        /*Returns a list of 'N' articles or a list of size = 0 if not articles are returned by a given query*/
         return articles;
     }
 
 
-    /*-----------END OF ARTICLE DAO--------------*/
+     /*------------------------------*/
+    /*End of Class*/
+    /*------------------------------*/
 }
